1c4a5919b375b1a0c6cc095e8debb5d8190e2096
Roger Perez <rogperez52@gmail.com>
2020-12-21 14:48:32 -0700

    Add create function on songs controller

--- backend/src/songs_controller.ts
+++ backend/src/songs_controller.ts
@@ -1,7 +1,19 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+'use strict';
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+  Callback,
+} from 'aws-lambda';
+import { DynamoDB, AWSError } from 'aws-sdk';
+import * as uuid from 'uuid';
+
+const db = new DynamoDB.DocumentClient();
 
 export const get = async function (
-  event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent,
+  context: Context
 ): Promise<APIGatewayProxyResult> {
   return Promise.resolve({
     statusCode: 200,
@@ -9,11 +21,35 @@ export const get = async function (
   });
 };
 
-export const create = async function (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> {
-  return Promise.resolve({
-    statusCode: 200,
-    body: 'Hello, world!',
+export const create = function (
+  event: APIGatewayProxyEvent,
+  _context: Context,
+  callback: Callback
+) {
+  const tableName = process.env.TABLE_NAME || '';
+  const data = JSON.parse(event.body || '');
+  const id = uuid.v1();
+  const params: DynamoDB.DocumentClient.PutItemInput = {
+    TableName: tableName,
+    Item: {
+      PK: `SONG#${id}`,
+      SK: `#METADATA#${id}`,
+      Title: data.title,
+    },
+  };
+
+  db.put(params, (err: AWSError, _res) => {
+    if (err) {
+      console.error(err);
+      callback(
+        new Error(`Could not persist item. [${err.message}] (${err.code})`)
+      );
+      return;
+    }
+
+    callback(null, {
+      statusCode: 200,
+      body: 'Item successfully created',
+    });
   });
 };
